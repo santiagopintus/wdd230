0efd1b3ee7f32f58e70709bf4f32a4bb48d3647c
Santiago Pintus <santiagopintus@gmail.com>
2021-11-24 17:06:32 -0300

    Uploaded week 11 Happy Winds Site

---
+++ lesson11/HappyWinds/js/cityInfo.js
@@ -0,0 +1,88 @@
+const prestonId = '5604473';
+const apiKey = "057e1b5ddc6ca4ac1e70d670cfd1c1fe";
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?id=${prestonId}&appid=${apiKey}&units=imperial`;
+
+const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?id=${prestonId}&appid=${apiKey}&units=imperial`;
+
+// Fetching the data from the api
+fetch(apiUrl)
+    .then((response) => response.json())
+    .then((json) => {
+        createHtml(json);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+function createHtml(response) {
+    console.log(response);
+    const currently_element = document.getElementById('currently');
+    const currently = response.weather[0].description;
+    currently_element.innerHTML = currently;
+
+    const temp_element = document.getElementById('temp');
+    const temp = response.main.temp;
+    temp_element.innerHTML = temp;
+
+    const speed_element = document.getElementById('speed');
+    const speed = response.wind.speed;
+    speed_element.innerHTML = speed;
+
+    const humidity_element = document.getElementById('humidity');
+    const humidity = response.main.humidity;
+    humidity_element.innerHTML = `${humidity}%`;
+
+    const windChill_element = document.getElementById('windChill');
+    const windChill = response.main.feels_like;
+    windChill_element.innerHTML = windChill;
+}
+
+fetch(forecastURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+        let days18pm = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));
+        const forecastContainer = document.getElementById('forecastContainer');
+        let isFirstDay = true;
+
+        //Loop to get just 5 results
+        for (let day = 0; day <= 4; day++) {
+            const dayDiv = document.createElement('DIV');
+            dayDiv.classList.add('one-day');
+
+            if (isFirstDay) {
+                dayDiv.classList.add('first-day');
+                isFirstDay = false;
+            }
+
+            const dayName_e = document.createElement('P');
+            dayName_e.classList.add('day-title');
+
+            const dayIcon_e = document.createElement('IMG')
+            const dayTemp_e = document.createElement('P')
+
+            let d = new Date(days18pm[day].dt_txt);
+            let dayTemp = days18pm[day].main.temp;
+
+            dayName_e.textContent = daysOfWeek[d.getDay()];
+            dayTemp_e.innerHTML = `${dayTemp} °F`;
+            
+            // Setting the icon
+            const imgalt = days18pm[day].weather[0].description;
+            const icon = days18pm[day].weather[0].icon
+            const src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
+            dayIcon_e.setAttribute('src', src);
+            dayIcon_e.setAttribute('alt', imgalt);
+
+            dayDiv.appendChild(dayName_e);
+            dayDiv.appendChild(dayIcon_e);
+            dayDiv.appendChild(dayTemp_e);
+
+            forecastContainer.appendChild(dayDiv);
+            forecastContainer.style.width = 'fit-content';
+
+            document.getElementById('loadingMessage').style.display = 'none'
+        }
+    })
+    
---
+++ lesson11/HappyWinds/js/fetchTowns.js
@@ -0,0 +1,53 @@
+const townsUrl = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(townsUrl)
+    .then((response) => response.json())
+    .then((jsonObject) => {
+        createTownsHtml(jsonObject);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+function createTownsHtml(jsonObject) {
+    const towns = jsonObject['towns'];
+    const townsContainer = document.querySelector('.towns');
+
+    for (let town of towns) {
+        if (town.name == 'Fish Haven' || town.name == 'Preston' || town.name == 'Soda Springs') {
+            let card = document.createElement('section');
+            let infoContainer = document.createElement('div');
+            let h2 = document.createElement('h2');
+            let motto = document.createElement('p');
+            let yearFounded = document.createElement('p');
+            let population = document.createElement('p');
+            let rainfall = document.createElement('p');
+            let photoContainer = document.createElement('div');
+            let photo = document.createElement('img');
+
+            card.classList.add('town');
+            infoContainer.classList.add('info-container');
+            h2.textContent = town.name;
+            motto.textContent = 'Motto: ' + town.motto;
+            yearFounded.textContent = 'Year Founded: ' + town.yearFounded;
+            population.textContent = 'Population: ' + town.currentPopulation;
+            rainfall.textContent = 'Annual Rain Fall: ' + town.averageRainfall;
+            photoContainer.classList.add('photo-container');
+            photo.setAttribute('src', `images/${town.photo}`);
+            photo.setAttribute('alt', `${town.name}`);
+            photoContainer.appendChild(photo);
+
+            infoContainer.appendChild(h2);
+            infoContainer.appendChild(motto);
+            infoContainer.appendChild(yearFounded);
+            infoContainer.appendChild(population);
+            infoContainer.appendChild(rainfall);
+            // photoContainer.appendChild(photo);
+
+            card.appendChild(infoContainer);
+            card.appendChild(photoContainer);
+
+            townsContainer.appendChild(card);
+        }
+    }
+}
